2688945102c3d4a47c09f36244f5eb38553b9a2f
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-19 06:11:50 +0200

    2 files modified, 1 added, 1 deleted

--- index.ts
+++ index.ts
@@ -7,3 +7,20 @@ of('World')
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
